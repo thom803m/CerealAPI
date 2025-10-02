60117c64373817cc13b75c2f60a39926d2384f78
SPAC-44 <tblancestremere@gmail.com>
2025-10-02 18:32:40 +0200

    Added sortering. Including new queries for both calories and sugars in order to the amount between the scales (Min(Max)

--- CerealAPI/Controllers/CerealsController.cs
+++ CerealAPI/Controllers/CerealsController.cs
@@ -20,23 +20,49 @@ namespace CerealAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Cereal>>> GetCereals(
             [FromQuery] string? mfr,
-            [FromQuery] int? calories,
-            [FromQuery] int? sugars)
+            [FromQuery] int? caloriesMin,
+            [FromQuery] int? caloriesMax,
+            [FromQuery] int? sugarsMin,
+            [FromQuery] int? sugarsMax,
+            [FromQuery] string? sort)
         {
             IQueryable<Cereal> query = _context.Cereals;
 
+            // Filtrering på producent
             if (!string.IsNullOrEmpty(mfr))
                 query = query.Where(c => c.Mfr == mfr);
 
-            if (calories.HasValue)
-                query = query.Where(c => c.Calories == calories.Value);
+            // Intervalbaseret filtrering på kalorier
+            if (caloriesMin.HasValue)
+                query = query.Where(c => c.Calories >= caloriesMin.Value);
+            if (caloriesMax.HasValue)
+                query = query.Where(c => c.Calories <= caloriesMax.Value);
 
-            if (sugars.HasValue)
-                query = query.Where(c => c.Sugars == sugars.Value);
+            // Intervalbaseret filtrering på sukker
+            if (sugarsMin.HasValue)
+                query = query.Where(c => c.Sugars >= sugarsMin.Value);
+            if (sugarsMax.HasValue)
+                query = query.Where(c => c.Sugars <= sugarsMax.Value);
+
+            // Sortering
+            if (!string.IsNullOrEmpty(sort))
+            {
+                query = sort.ToLower() switch
+                {
+                    "calories_asc" => query.OrderBy(c => c.Calories),
+                    "calories_desc" => query.OrderByDescending(c => c.Calories),
+                    "sugars_asc" => query.OrderBy(c => c.Sugars),
+                    "sugars_desc" => query.OrderByDescending(c => c.Sugars),
+                    "rating_asc" => query.OrderBy(c => c.Rating),
+                    "rating_desc" => query.OrderByDescending(c => c.Rating),
+                    _ => query
+                };
+            }
 
             var result = await query.ToListAsync();
             return Ok(result);
         }
+
 
 
         // GET: api/cereals/5
